3d0e1ff2420b94beb43a85c7db8799c9842c107c
Joseph Lee <jiosephlee@gmail.com>
2026-02-10 01:52:20 -0500

    debug ray

--- openrlhf/cli/train_ppo_ray.py
+++ openrlhf/cli/train_ppo_ray.py
@@ -18,7 +18,7 @@ from openrlhf.utils import get_strategy
 def train(args):
     # initialize ray if not initialized
     if not ray.is_initialized():
-        ray.init(runtime_env={"env_vars": {"TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "WARN"}})
+        ray.init(runtime_env={"env_vars": {"TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "INFO"}})
 
     # configure strategy
     strategy = get_strategy(args)

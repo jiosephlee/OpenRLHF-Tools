e7b510e65dc40bfd5dcce2cbce8ae758fc533b81
Joseph Lee <jiosephlee@gmail.com>
2026-02-10 19:36:44 -0500

    fix tool calling; stop tokens

--- openrlhf/trainer/ppo_utils/experience_maker.py
+++ openrlhf/trainer/ppo_utils/experience_maker.py
@@ -369,6 +369,8 @@ class SamplesGenerator:
             max_tokens=generate_kwargs.get("max_new_tokens", 1024),
             min_tokens=generate_kwargs.get("min_new_tokens", 1),
             skip_special_tokens=generate_kwargs.get("skip_special_tokens", False),
+            stop=self.args.vllm_stop_strings,
+            include_stop_str_in_output=True,
             logprobs=1 if self.args.enable_vllm_is_correction else None,
         )
         truncate_length = generate_kwargs.get("prompt_max_len", 1024) + generate_kwargs.get("max_new_tokens", 1024)

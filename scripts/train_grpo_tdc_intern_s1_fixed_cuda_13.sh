fe629f463948473c2641de779c73245a53d3b8a5
Joseph Lee <jiosephlee@gmail.com>
2026-02-11 20:43:03 -0500

    try interaactive on colocate

--- scripts/train_grpo_tdc_intern_s1_fixed_cuda_13.sh
+++ scripts/train_grpo_tdc_intern_s1_fixed_cuda_13.sh
@@ -113,7 +113,7 @@ run_task() {
     DELETE_LOCAL_AFTER_PUSH=true
 
     # Training hyperparameters
-    TRAIN_BATCH_SIZE=$((NUM_GPUS * 16))
+    TRAIN_BATCH_SIZE=$((NUM_GPUS * 4))
     NUM_GPUS=$SLURM_GPUS_ON_NODE
     VLLM_NUM_ENGINES=$NUM_GPUS
 
@@ -279,13 +279,13 @@ run_task() {
         --save_steps -1 \
         --logging_steps 1 \
         --n_samples_per_prompt $N_SAMPLES_PER_PROMPT \
-        --micro_train_batch_size 8 \
-        --micro_rollout_batch_size 16 \
+        --micro_train_batch_size 2 \
+        --micro_rollout_batch_size 4 \
         --train_batch_size $TRAIN_BATCH_SIZE \
         --rollout_batch_size $TRAIN_BATCH_SIZE \
         --max_epochs 1 \
-        --prompt_max_len 4096 \
-        --generate_max_len 8192 \
+        --prompt_max_len 8192 \
+        --generate_max_len 2048 \
         --max_samples 1000000 \
         --zero_stage 1 \
         --param_dtype bf16 \

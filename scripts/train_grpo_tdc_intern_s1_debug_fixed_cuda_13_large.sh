eff3fb3cc66f5b098ffcdcd289fc413c8f86e685
Joseph Lee <jiosephlee@gmail.com>
2026-02-13 16:14:33 -0500

    validate regular math recipe

--- scripts/train_grpo_tdc_intern_s1_debug_fixed_cuda_13_large.sh
+++ scripts/train_grpo_tdc_intern_s1_debug_fixed_cuda_13_large.sh
@@ -25,18 +25,18 @@ DEBUG_TRACES=${3:-"0"}
 
 ### MULTI-TASK: AMES, BBB_Martins, Bioavailability_Ma, hERG ###
 TASK_NAMES=(DILI CYP2D6_Substrate_CarbonMangels ClinTox BBB_Martins Bioavailability_Ma hERG)
-TASK_LABEL="multi4"
+TASK_LABEL="Base"
 
 # ### NCCL / IB / NETWORK CONFIG ###
-# export OMP_NUM_THREADS=16
-# export NCCL_NVLS_ENABLE=1
-# export NCCL_IB_ADAPTIVE_ROUTING=1
-# export NCCL_IB_SL=1
-# export NCCL_IB_QPS_PER_CONNECTION=2
-# export NCCL_IB_SPLIT_DATA_ON_QPS=0
-# export NCCL_IB_HCA=mlx5_15,mlx5_10,mlx5_14,mlx5_13,mlx5_8,mlx5_7,mlx5_9,mlx5_4
-# export NCCL_SOCKET_IFNAME=bond0
-# export UCX_TLS=rc
+export OMP_NUM_THREADS=16
+export NCCL_NVLS_ENABLE=1
+export NCCL_IB_ADAPTIVE_ROUTING=1
+export NCCL_IB_SL=1
+export NCCL_IB_QPS_PER_CONNECTION=2
+export NCCL_IB_SPLIT_DATA_ON_QPS=0
+export NCCL_IB_HCA=mlx5_15,mlx5_10,mlx5_14,mlx5_13,mlx5_8,mlx5_7,mlx5_9,mlx5_4
+export NCCL_SOCKET_IFNAME=bond0
+export UCX_TLS=rc
 
 ### W&B ###
 if [ -z "${WANDB_API_KEY:-}" ]; then

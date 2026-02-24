dbcca30d19e8952453622dce6af211588e2ed8b4
Joseph Lee <jiosephlee@gmail.com>
2026-02-24 01:34:23 -0500

    .

--- scripts/train_grpo_tdc_gpt_oss_v3_tools_FINAL_MXP4.sh
+++ scripts/train_grpo_tdc_gpt_oss_v3_tools_FINAL_MXP4.sh
@@ -190,7 +190,7 @@ python -m openrlhf.cli.train_ppo_ray \
     --vllm_num_engines $VLLM_NUM_ENGINES \
     --vllm_tensor_parallel_size 1 \
     --colocate_all_models \
-    --vllm_gpu_memory_utilization 0.6 \
+    --vllm_gpu_memory_utilization 0.5 \
     --advantage_estimator $ADVANTAGE_ESTIMATOR \
     --init_kl_coef 0 \
     --kl_estimator k1 \

b27cbd4a3d345fd30af4cca81d62b5db45b026f2
Joseph Lee <jiosephlee@gmail.com>
2026-02-25 20:25:22 -0500

    fill in missing data

--- scripts/train_grpo_tdc_gpt_oss_unsloth.sh
+++ scripts/train_grpo_tdc_gpt_oss_unsloth.sh
@@ -31,9 +31,6 @@ eval "$(conda shell.bash hook)"
 conda activate /vast/projects/myatskar/design-documents/conda_env/open_rlhf_intern # This conda env uses torch 2.9.1, and the corresponding flash-attn for cuda 13.1.0, but torch is compiled for cuda 12.8... torch doesn't have pip wheels for 13.1.0 yet; no problems with this for now except for Adam_offload.
 set -euo pipefail
 export MALLOC_TRIM_THRESHOLD_=0
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export VLLM_ENABLE_V1_MULTIPROCESSING=0
-export VLLM_CUDAGRAPH_CAPTURE_SIZES="1,2,4,8,16,32"
 export DS_SKIP_CUDA_CHECK=1 # Adam_offload checks CUDA version and which version of torch is compiled for it; this is a workaround to skip the CUDA check.
 # export VLLM_USE_FLASHINFER_MOE_MXFP4_MXFP8=1
 
